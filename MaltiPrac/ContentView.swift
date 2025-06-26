7ec885ea17b86b7acd7e19f38697fe88105a4363
BL-CZY <jiguangllsw@gmail.com>
2025-06-26 17:57:03 +0200

    Init

---
+++ MaltiPrac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaltiPrac
+//
+//  Created by huang fanhua on 26/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaltiPrac/MaltiPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaltiPracApp.swift
+//  MaltiPrac
+//
+//  Created by huang fanhua on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MaltiPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
